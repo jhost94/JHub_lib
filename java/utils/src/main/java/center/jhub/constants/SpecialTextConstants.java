23ec78396e0532897974b366b7304474f8cbd5eb
Jhost <jhost94@gmail.com>
2026-06-23 17:15:56 +0100

    Adds special string constants

---
+++ java/utils/src/main/java/center/jhub/constants/SpecialTextConstants.java
@@ -0,0 +1,46 @@
+package center.jhub.constants;
+
+public class SpecialTextConstants {
+
+    /**
+     * Commonly used special text constants for string manipulation and formatting.
+     */
+    public static final String EMPTY_STRING = "";
+    public static final String SPACE = " ";
+    public static final String COMMA = ",";
+    public static final String COLON = ":";
+    public static final String SEMICOLON = ";";
+
+    /**
+     * Special characters for showing and creating tables
+     */
+
+    public static final String HORIZONTAL_LINE = "─";
+    public static final String VERTICAL_LINE = "│";
+    public static final String TOP_LEFT_CORNER = "┌";
+    public static final String TOP_RIGHT_CORNER = "┐";
+    public static final String BOTTOM_LEFT_CORNER = "└";
+    public static final String BOTTOM_RIGHT_CORNER = "┘";
+
+    public static final String TEE_LEFT = "├";
+    public static final String TEE_RIGHT = "┤";
+    public static final String TEE_TOP = "┬";
+    public static final String TEE_BOTTOM = "┴";
+    public static final String CROSS = "┼";
+
+    public static final String ARROW_UP = "↑";
+    public static final String ARROW_DOWN = "↓";
+    public static final String ARROW_LEFT = "←";
+    public static final String ARROW_RIGHT = "→";
+    public static final String ARROW_UP_DOWN = "↕";
+    public static final String ARROW_LEFT_RIGHT = "↔";
+    public static final String ARROW_UP_RIGHT = "↗";
+    public static final String ARROW_UP_LEFT = "↖";
+    public static final String ARROW_DOWN_RIGHT = "↘";
+    public static final String ARROW_DOWN_LEFT = "↙";
+
+    public static final String ARROW_DOWN_FILLED = "▼";
+    public static final String ARROW_UP_FILLED = "▲";
+    public static final String ARROW_LEFT_FILLED = "◀";
+    public static final String ARROW_RIGHT_FILLED = "▶";
+}
